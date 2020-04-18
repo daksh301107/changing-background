7169ab0572c3d1dd6f8f0dc55c9dfb9d742882da
daksh301107 <63402362+daksh301107@users.noreply.github.com>
2020-04-18 07:58:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+var r = 0;
+var g = 50;
+var b=255;
+
+// INITIALIZE ALL YOUR VARIABLES
+// Ideally you would create 3 variables. red blue green or r, g, b
+
+
+function setup(){
+
+  createCanvas(1200,400);
+}
+
+
+function draw(){
+  
+  r=map(mouseX,0,1200,0,255);
+  g=map(mouseX,0,1200,125,255);
+  b=map(mouseX,0,1200,0,125);
+  background(r,g,b);
+  fill(250,118,222);
+  ellipse(mouseX,200,30,40);
+  // change the value of Red based on the mouse movement about the X axis
+  // change the value of Green based on the mouse movement about the X axis
+  // change the value of Blue based on the mouse movement about the X axis
+
+  // Use the map() function to do so. 
+
+  // Pass the values to the background() function you have learnt previously.
+
+  // Create an ellipse that will move around with you mouse about the X axis.
+  // Remember to fill the canvas with white paint before creating the ellipse.
+}
